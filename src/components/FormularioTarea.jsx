46711a20e4a156489363e7df37c779d19ad7ae56
Emilse Arias <emilsearias@gmail.com>
2023-12-19 11:35:30 -0300

    agregamos los componentes de la lista de tareas y un item para la lista

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -1,4 +1,5 @@
 import { Button, Form } from "react-bootstrap";
+import ListaTareas from "./ListaTareas";
 // import Form from "react-bootstrap/Form";
 // import Button from "react-bootstrap/Button";
 
@@ -11,6 +12,7 @@ const FormularioTarea = () => {
           <Button variant="info" type="submit">Agregar</Button>
         </Form.Group>
       </Form>
+      <ListaTareas></ListaTareas>
     </section>
   );
 };
