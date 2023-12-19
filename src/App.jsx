35132748e3178c030c688c1b449ceca37500daae
Emilse Arias <emilsearias@gmail.com>
2023-12-18 21:53:03 -0300

    Agregamos el componente del formulario y footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
+import { Container } from "react-bootstrap";
 import "./App.css";
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import Footer from "./components/Footer";
 
 function App() {
-  return <>
-  <h1 className="text-center">Lista de tareas</h1>
-  </>;
+  return (
+    <>
+      <Container className="my-4">
+        <h1 className="text-center text-light display-3">Lista de tareas</h1>
+      </Container>
+      <Footer></Footer>
+    </>
+  );
 }
 
 export default App;
