d5356291bd2c7b5cc3126b1c9fb14afdee7a353f
Emilse Arias <emilsearias@gmail.com>
2023-12-21 10:36:59 -0300

    agregamos las tareas en el state arrayTareas

--- src/components/FormularioTarea.jsx
+++ src/components/FormularioTarea.jsx
@@ -8,15 +8,24 @@ const FormularioTarea = () => {
   const [tarea, setTarea] = useState("");
   const [arrayTareas, setArrayTareas] = useState([]);
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    //guardar el state tarea en el arrayTareas
+    // spread ...
+    setArrayTareas([...arrayTareas, tarea])
+   //limpiar el formulario
+   setTarea('');
+  };
+
   return (
     <section>
-      <Form>
+      <Form onSubmit={handleSubmit}>
         <Form.Group className="mb-3 d-flex" controlId="formularioTarea">
           <Form.Control
             type="text"
             placeholder="Ej: tarea 1"
             className="me-2"
-            onChange={(e)=> setTarea(e.target.value) }
+            onChange={(e) => setTarea(e.target.value)}
             value={tarea}
           />
           <Button variant="info" type="submit">
